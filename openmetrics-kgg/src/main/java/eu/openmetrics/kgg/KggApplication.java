00ff9854dbe8b862549bd71653ee900881ed9b2c
Kyriakos <Kyriakos@192.168.10.7>
2021-06-29 19:24:54 +0300

    test

--- openmetrics-kgg/src/main/java/eu/openmetrics/kgg/KggApplication.java
+++ openmetrics-kgg/src/main/java/eu/openmetrics/kgg/KggApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OpenmetricsKggApplication {
+public class KggApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OpenmetricsKggApplication.class, args);
+		SpringApplication.run(KggApplication.class, args);
 	}
 
 }
