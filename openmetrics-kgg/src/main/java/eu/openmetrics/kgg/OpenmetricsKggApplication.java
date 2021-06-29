2131f09bd0cc02f5ee8e420015d742a5cb1e2b8c
Kyriakos <Kyriakos@192.168.10.7>
2021-06-29 19:22:15 +0300

    Initial commit

---
+++ openmetrics-kgg/src/main/java/eu/openmetrics/kgg/OpenmetricsKggApplication.java
@@ -0,0 +1,13 @@
+package eu.openmetrics.kgg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenmetricsKggApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenmetricsKggApplication.class, args);
+	}
+
+}
