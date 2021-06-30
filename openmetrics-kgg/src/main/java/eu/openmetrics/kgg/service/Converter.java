b18481af1ccb08688292b4b85a20746b4f6d36c2
Kyriakos <Kyriakos@192.168.10.7>
2021-06-30 13:58:15 +0300

    ...

--- openmetrics-kgg/src/main/java/eu/openmetrics/kgg/service/Converter.java
+++ openmetrics-kgg/src/main/java/eu/openmetrics/kgg/service/Converter.java
@@ -2,21 +2,34 @@ package eu.openmetrics.kgg.service;
 
 import java.util.Iterator;
 import java.util.List;
-
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.vocabulary.RDF;
+import org.apache.jena.vocabulary.RDFS;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import gr.tuc.ifc.IfcModel;
+import gr.tuc.ifc4.IfcBeam;
 import gr.tuc.ifc4.IfcBuilding;
 import gr.tuc.ifc4.IfcBuildingStorey;
+import gr.tuc.ifc4.IfcColumn;
+import gr.tuc.ifc4.IfcDoor;
 import gr.tuc.ifc4.IfcObjectDefinition;
+import gr.tuc.ifc4.IfcOpeningElement;
 import gr.tuc.ifc4.IfcProduct;
 import gr.tuc.ifc4.IfcProject;
 import gr.tuc.ifc4.IfcRelAggregates;
 import gr.tuc.ifc4.IfcRelContainedInSpatialStructure;
+import gr.tuc.ifc4.IfcRelFillsElement;
+import gr.tuc.ifc4.IfcRelVoidsElement;
+import gr.tuc.ifc4.IfcRoof;
 import gr.tuc.ifc4.IfcSite;
+import gr.tuc.ifc4.IfcSlab;
+import gr.tuc.ifc4.IfcSpace;
+import gr.tuc.ifc4.IfcWall;
+import gr.tuc.ifc4.IfcWallStandardCase;
+import gr.tuc.ifc4.IfcWindow;
 
 @Service
 public class Converter {
@@ -29,14 +42,18 @@ public class Converter {
 		
 	public void convert(IfcModel ifcModel) {
 		Model rdfModel = ModelFactory.createDefaultModel();
-		
+		rdfModel.setNsPrefix("om", "http://openmetrics.eu/openmetrics#");
+		rdfModel.setNsPrefix("owl", "http://www.w3.org/2002/07/owl#");
+		rdfModel.setNsPrefix("rdf", RDF.uri);
+		rdfModel.setNsPrefix("rdfs", RDFS.uri);
+		rdfModel.setNsPrefix("schema", "http://schema.org#");
+		rdfModel.setNsPrefix("brick", "https://brickschema.org/schema/1.1/Brick#");
 		
 		Iterator<IfcProject> projectIterator = ifcModel.getAllE(IfcProject.class).iterator();
 		if(projectIterator.hasNext()){
 			IfcProject ifcProject = projectIterator.next();
 			//
 			//
-			
 			Iterator<IfcRelAggregates> projectRelAggregatesIterator = ifcProject.getIsDecomposedBy().iterator();
 			if(projectRelAggregatesIterator.hasNext()) {
 				List<IfcObjectDefinition> projectRelatedObjectList = projectRelAggregatesIterator.next().getRelatedObjects();
@@ -50,23 +67,16 @@ public class Converter {
 							List<IfcObjectDefinition> siteRelatedObjectList = siteRelAggregatesIterator.next().getRelatedObjects();
 							for(IfcObjectDefinition siteRelatedObject : siteRelatedObjectList) {
 								if(siteRelatedObject instanceof IfcBuilding) {
-									IfcBuilding ifcBuilding = (IfcBuilding) projectRelatedObject;
-											
+									IfcBuilding building = (IfcBuilding) projectRelatedObject;
+									parseBuilding(building);
 								}else {
 									log.warn("unsupported case");
 								}
 							}
-						}
-						
-						
-									
+						}		
 					}else if (projectRelatedObject instanceof IfcBuilding) {
-						IfcBuilding ifcBuilding = (IfcBuilding) projectRelatedObject;
-						//
-						//
-						
-						
-						
+						IfcBuilding building = (IfcBuilding) projectRelatedObject;
+						parseBuilding(building);					
 					}else {
 						log.warn("usupported case");
 					}
@@ -75,10 +85,18 @@ public class Converter {
 		}		
 	}
 	
-	private void parseBuilding(IfcBuilding ifcBuilding) {
-		//
-		//
-		Iterator<IfcRelAggregates> buildingRelAggregatesIterator = ifcBuilding.getIsDecomposedBy().iterator();
+	private void parseBuilding(IfcBuilding building) {
+		// 1. using containedInSpatialStructure
+		Iterator<IfcRelContainedInSpatialStructure> buildingContainedInSpatialStructureIterator = building.getContainsElements().iterator();
+		while(buildingContainedInSpatialStructureIterator.hasNext()) {
+			IfcRelContainedInSpatialStructure relContainedInSpatialStructure = buildingContainedInSpatialStructureIterator.next();
+			for(IfcProduct product : relContainedInSpatialStructure.getRelatedElements()) {
+				// -->
+				parseProduct(product);
+			}
+		}
+		// 2. using decomposedBy
+		Iterator<IfcRelAggregates> buildingRelAggregatesIterator = building.getIsDecomposedBy().iterator();
 		if(buildingRelAggregatesIterator.hasNext()) {
 			List<IfcObjectDefinition> buildingRelatedObjectList = buildingRelAggregatesIterator.next().getRelatedObjects();
 			for(IfcObjectDefinition builidngRelatedObject : buildingRelatedObjectList) {
@@ -90,15 +108,12 @@ public class Converter {
 					if(buildingStoreyRelAggregatesIterator.hasNext()) {
 						List<IfcObjectDefinition> buildingStoreyRelatedObjectList = buildingStoreyRelAggregatesIterator.next().getRelatedObjects();
 						for(IfcObjectDefinition buildingStoreyRelatedObject : buildingStoreyRelatedObjectList) {
-							//
-							//
 							if(buildingStoreyRelatedObject instanceof IfcProduct) {
 								IfcProduct product = (IfcProduct) buildingStoreyRelatedObject;
-								//
-								//
-								
+								// -->						
+								parseProduct(product);
 							}else {
-								// we exclude IfcActor, IfcControl, IfcProcess etc.
+								// we exclude some entities
 								log.warn("unsupported case");
 							}
 						}
@@ -108,9 +123,8 @@ public class Converter {
 					while(buildingStoreyContainedInSpatialStructureIterator.hasNext()) {
 						IfcRelContainedInSpatialStructure relContainedInSpatialStructure = buildingStoreyContainedInSpatialStructureIterator.next();
 						for(IfcProduct product : relContainedInSpatialStructure.getRelatedElements()) {
-							//
-							//
-							
+							// -->
+							parseProduct(product);
 						}
 					}					
 				}else {
@@ -120,4 +134,70 @@ public class Converter {
 		}
 	}
 	
+	private void parseProduct(IfcProduct product) {
+		
+		if(product instanceof IfcWall) {
+			IfcWall ifcWall = (IfcWall) product;
+			//
+			//
+			Iterator<IfcRelVoidsElement> wallOpeningIterator = ifcWall.getHasOpenings().iterator();
+			if(wallOpeningIterator.hasNext()) {
+				IfcRelVoidsElement relVoidsElement = wallOpeningIterator.next();
+				if(relVoidsElement.getRelatedOpeningElement() instanceof IfcOpeningElement) {
+					IfcOpeningElement opening = (IfcOpeningElement) relVoidsElement.getRelatedOpeningElement();
+					// -->
+					parseOpening(opening);
+				}else {
+					log.warn("unsupported case");
+				}
+			}
+		}else if(product instanceof IfcWallStandardCase) {
+			IfcWallStandardCase ifcWallStandardCase = (IfcWallStandardCase) product;
+			//
+			//
+			
+		}else if(product instanceof IfcSlab) {
+			IfcSlab ifcSlab = (IfcSlab) product;
+			//
+			//
+			
+		}else if(product instanceof IfcSpace) {
+			IfcSpace ifcSpace = (IfcSpace) product;
+						
+		}else if(product instanceof IfcColumn) {
+			IfcColumn ifcColumn = (IfcColumn) product;
+						
+		}else if(product instanceof IfcBeam) {
+			IfcBeam ifcBeam = (IfcBeam) product;
+			
+		}else if(product instanceof IfcRoof) {
+			IfcRoof ifcRoof = (IfcRoof) product;			
+			
+			// 1. hasOpenings
+			
+			
+			// 2. isDecomposedBy (e.g. slabs)
+			
+		}
+	}
+	
+	public void parseOpening(IfcOpeningElement opening) {
+		Iterator<IfcRelFillsElement> relFillsElementIterator = opening.getHasFillings().iterator();
+		if(relFillsElementIterator.hasNext()) {
+			IfcRelFillsElement relFillsElement = relFillsElementIterator.next();
+			if(relFillsElement.getRelatedBuildingElement() instanceof IfcDoor) {
+				IfcDoor door = (IfcDoor) relFillsElement.getRelatedBuildingElement();
+				//
+				//
+				
+			}else if(relFillsElement.getRelatedBuildingElement() instanceof IfcWindow) {				
+				IfcWindow window = (IfcWindow) relFillsElement.getRelatedBuildingElement();
+				//
+				//
+				
+			}else {
+				log.warn("unsupported case");
+			}
+		}
+	}
 }
