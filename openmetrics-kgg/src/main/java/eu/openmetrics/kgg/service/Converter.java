6ce2df26169e852799efef91b5476dc347f0a64d
Kyriakos <Kyriakos@192.168.10.7>
2021-06-30 13:10:49 +0300

    ...

--- openmetrics-kgg/src/main/java/eu/openmetrics/kgg/service/Converter.java
+++ openmetrics-kgg/src/main/java/eu/openmetrics/kgg/service/Converter.java
@@ -10,9 +10,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import gr.tuc.ifc.IfcModel;
 import gr.tuc.ifc4.IfcBuilding;
+import gr.tuc.ifc4.IfcBuildingStorey;
 import gr.tuc.ifc4.IfcObjectDefinition;
+import gr.tuc.ifc4.IfcProduct;
 import gr.tuc.ifc4.IfcProject;
 import gr.tuc.ifc4.IfcRelAggregates;
+import gr.tuc.ifc4.IfcRelContainedInSpatialStructure;
 import gr.tuc.ifc4.IfcSite;
 
 @Service
@@ -36,27 +39,85 @@ public class Converter {
 			
 			Iterator<IfcRelAggregates> projectRelAggregatesIterator = ifcProject.getIsDecomposedBy().iterator();
 			if(projectRelAggregatesIterator.hasNext()) {
-				IfcRelAggregates projectRelAggregates = projectRelAggregatesIterator.next();
-				List<IfcObjectDefinition> relatedObjectList = projectRelAggregates.getRelatedObjects();
-				for(IfcObjectDefinition relatedObject : relatedObjectList) {
-					if(relatedObject instanceof IfcSite) {
-						IfcSite ifcSite = (IfcSite) relatedObject;
+				List<IfcObjectDefinition> projectRelatedObjectList = projectRelAggregatesIterator.next().getRelatedObjects();
+				for(IfcObjectDefinition projectRelatedObject : projectRelatedObjectList) {
+					if(projectRelatedObject instanceof IfcSite) {
+						IfcSite ifcSite = (IfcSite) projectRelatedObject;
 						//
 						//
-					
+						Iterator<IfcRelAggregates> siteRelAggregatesIterator = ifcSite.getIsDecomposedBy().iterator();
+						if(siteRelAggregatesIterator.hasNext()) {
+							List<IfcObjectDefinition> siteRelatedObjectList = siteRelAggregatesIterator.next().getRelatedObjects();
+							for(IfcObjectDefinition siteRelatedObject : siteRelatedObjectList) {
+								if(siteRelatedObject instanceof IfcBuilding) {
+									IfcBuilding ifcBuilding = (IfcBuilding) projectRelatedObject;
+											
+								}else {
+									log.warn("unsupported case");
+								}
+							}
+						}
 						
-					}else if (relatedObject instanceof IfcBuilding) {
+						
+									
+					}else if (projectRelatedObject instanceof IfcBuilding) {
+						IfcBuilding ifcBuilding = (IfcBuilding) projectRelatedObject;
+						//
 						//
 						
 						
 						
 					}else {
-						
 						log.warn("usupported case");
-						
 					}
 				}
 			}
 		}		
 	}
+	
+	private void parseBuilding(IfcBuilding ifcBuilding) {
+		//
+		//
+		Iterator<IfcRelAggregates> buildingRelAggregatesIterator = ifcBuilding.getIsDecomposedBy().iterator();
+		if(buildingRelAggregatesIterator.hasNext()) {
+			List<IfcObjectDefinition> buildingRelatedObjectList = buildingRelAggregatesIterator.next().getRelatedObjects();
+			for(IfcObjectDefinition builidngRelatedObject : buildingRelatedObjectList) {
+				if(builidngRelatedObject instanceof IfcBuildingStorey) {
+					IfcBuildingStorey buildingStorey = (IfcBuildingStorey) builidngRelatedObject;
+					//
+					// 1. using decomposedBy
+					Iterator<IfcRelAggregates> buildingStoreyRelAggregatesIterator = buildingStorey.getIsDecomposedBy().iterator(); 
+					if(buildingStoreyRelAggregatesIterator.hasNext()) {
+						List<IfcObjectDefinition> buildingStoreyRelatedObjectList = buildingStoreyRelAggregatesIterator.next().getRelatedObjects();
+						for(IfcObjectDefinition buildingStoreyRelatedObject : buildingStoreyRelatedObjectList) {
+							//
+							//
+							if(buildingStoreyRelatedObject instanceof IfcProduct) {
+								IfcProduct product = (IfcProduct) buildingStoreyRelatedObject;
+								//
+								//
+								
+							}else {
+								// we exclude IfcActor, IfcControl, IfcProcess etc.
+								log.warn("unsupported case");
+							}
+						}
+					}
+					// 2. using containedInSpatialStructure
+					Iterator<IfcRelContainedInSpatialStructure> buildingStoreyContainedInSpatialStructureIterator = buildingStorey.getContainsElements().iterator();
+					while(buildingStoreyContainedInSpatialStructureIterator.hasNext()) {
+						IfcRelContainedInSpatialStructure relContainedInSpatialStructure = buildingStoreyContainedInSpatialStructureIterator.next();
+						for(IfcProduct product : relContainedInSpatialStructure.getRelatedElements()) {
+							//
+							//
+							
+						}
+					}					
+				}else {
+					log.warn("unsupported case");
+				}
+			}
+		}
+	}
+	
 }
