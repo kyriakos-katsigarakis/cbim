dd2dfd7c695fe3d4850f1a14c6fbead2091dc9dc
Kyriakos <Kyriakos@192.168.10.7>
2021-07-02 19:47:15 +0300

    ...

--- openmetrics-kgg/src/main/java/eu/openmetrics/kgg/controller/MainController.java
+++ openmetrics-kgg/src/main/java/eu/openmetrics/kgg/controller/MainController.java
@@ -16,6 +16,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,7 +27,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
-
 import eu.openmetrics.kgg.service.Converter;
 import gr.tuc.ifc.IfcModel;
 
@@ -43,31 +45,58 @@ public class MainController {
 	@GetMapping("/")
 	public ModelAndView main(HttpSession httpSession) {
 		log.info("main page");
+		httpSession.setMaxInactiveInterval(1800);
 		ModelAndView modelView = new ModelAndView();
 		modelView.setViewName("page-main");
 		return modelView;
 	}
 
+	@GetMapping("/download")
+	public ResponseEntity<Resource> download(HttpSession httpSession) {
+		Path filePath = Paths.get(directory + File.separator + httpSession.getAttribute("fileName") + ".ttl");
+		try {
+			Resource fileResource = new UrlResource(filePath.toUri());
+			if(fileResource.exists()) {
+				return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + httpSession.getAttribute("fileName") + ".ttl\"").body(fileResource);
+			}else {
+				return ResponseEntity.ok().build();
+			}
+		} catch (Exception e) {
+			return ResponseEntity.ok().build();
+		}
+	}
+
 	@RequestMapping(value="/upload", method=RequestMethod.POST)
 	public ResponseEntity<String> upload(@RequestParam("file") MultipartFile multipartFile, HttpSession httpSession) throws IOException {
 		String fileName = FilenameUtils.getBaseName(multipartFile.getOriginalFilename());
+		String fileExtension = FilenameUtils.getExtension(multipartFile.getOriginalFilename());
 		String fileNameWithExtension = FilenameUtils.getName(multipartFile.getOriginalFilename());
-		Path folderPath = Paths.get(directory).toAbsolutePath().normalize();
-		Files.createDirectories(folderPath);
-		Path filePath = folderPath.resolve(fileNameWithExtension);
-		InputStream fileInputStream = multipartFile.getInputStream();
-		OutputStream fileOutputStream = Files.newOutputStream(filePath);		
-		IOUtils.copy(fileInputStream,fileOutputStream);
-		fileInputStream.close();
-		fileOutputStream.close();
-		IfcModel ifcModel = new IfcModel();
-		ifcModel.readFile(multipartFile.getInputStream());
-		File rdfFile = new File(directory + File.separator + fileName + ".ttl");
-		rdfFile.createNewFile();
-		FileOutputStream rdfFileOutputStream = new FileOutputStream(rdfFile, false);
-		Model rdfModel = converter.convert(ifcModel);
-		rdfModel.write(rdfFileOutputStream, "ttl");
-		rdfFileOutputStream.close();
-		return ResponseEntity.ok().body("ok");
+		if(fileExtension.equalsIgnoreCase("ifc")){
+			httpSession.setAttribute("fileName", fileName);	
+			Path folderPath = Paths.get(directory).toAbsolutePath().normalize();
+			Files.createDirectories(folderPath);
+			Path filePath = folderPath.resolve(fileNameWithExtension);
+			InputStream fileInputStream = multipartFile.getInputStream();
+			OutputStream fileOutputStream = Files.newOutputStream(filePath);		
+			IOUtils.copy(fileInputStream,fileOutputStream);
+			fileInputStream.close();
+			fileOutputStream.close();
+			IfcModel ifcModel = new IfcModel();
+			ifcModel.readFile(multipartFile.getInputStream());
+			String ifcSchema = ifcModel.getSchema();
+			if(ifcSchema.equalsIgnoreCase("ifc4")) {
+				File rdfFile = new File(directory + File.separator + fileName + ".ttl");
+				rdfFile.createNewFile();
+				FileOutputStream rdfFileOutputStream = new FileOutputStream(rdfFile, false);
+				Model rdfModel = converter.convert(ifcModel);
+				rdfModel.write(rdfFileOutputStream, "ttl");
+				rdfFileOutputStream.close();
+				return ResponseEntity.ok().body("success");
+			}else {
+				return ResponseEntity.ok().body("failed_schema");
+			}
+		}else {
+			return ResponseEntity.ok().body("failed_extension");
+		}
 	}
 }
--- openmetrics-kgg/src/main/resources/static/assets/custom/js/custom.js
+++ openmetrics-kgg/src/main/resources/static/assets/custom/js/custom.js
@@ -1,3 +1,39 @@
-$("#btn-form-upload").click(function(){
-	$("#form-upload").submit();	
+$(document).ready(function () {
+
+   	$("#btn-download").hide();
+
+	$("#btn-download").click(function () {
+		location.href = '/download';
+	});
+
+    $("#file").on('change', function(event) {
+		event.preventDefault();
+        var form = $('#form-upload')[0];
+        var data = new FormData(form);
+        $("#btn-form-upload").prop("disabled", true);
+        $.ajax({
+            type: "POST",
+            enctype: "multipart/form-data",
+            url: "/upload",
+            data: data,
+            processData: false,
+            contentType: false,
+            cache: false,
+            timeout: 600000,
+            success: function (data) {
+                console.log(data);
+               	if(data == "success"){
+	 				$("#btn-download").show();
+				}else if(data == "failed_extension"){
+					
+				}else if(data == "failed_schema"){
+					
+				}
+                $("#file").val(null);
+            },
+            error: function (e) {
+                console.log(e);
+            }
+        });
+	});
 });
